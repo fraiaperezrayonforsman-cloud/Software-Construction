88dbfab19dd97543f2daa3dbb931e4b62420f41b
Julie <julietruc.dao@uzh.ch>
2025-10-07 13:02:26 +0200

    Implementing Subclass Light

--- SoCo_HS25-group_004-a1/smart_house.py
+++ SoCo_HS25-group_004-a1/smart_house.py
@@ -1,8 +1,8 @@
 Device = {
     "get_power_consumption": get_power_consumption,
     "describe_device": describe_device,
-    "toggle_stats": toggle_status,
-    "_classname": Device
+    "toggle_status": toggle_status,
+    "_classname": "Device"
 }
 
 def device_new(name, location, basepower, status):
@@ -11,6 +11,34 @@ def device_new(name, location, basepower, status):
         "location": location,
         "basepower": basepower,
         "status": status,
-        "_class": "Device"
+        "_class": Device
     }
 
+
+
+
+
+def light_consumption(self):
+    if self["status"] != "on":
+        return 0
+    return round(self["basepower"] * (self["brightness"]/100))
+
+def light_description(self):
+    return f"The {self["name"]} is located in the {self["location"]}, is currently {self["status"]} and is currently set to {self["brightness"]}% brightness."
+
+Light = {
+    "get_power_consumption": light_consumption,
+    "describe_device": light_description,
+    "_classname": "Light",
+    "_parent": Device
+}
+
+def light_new(name, location, basepower, status, brightness):
+    return {
+        "name": name,
+        "location": location,
+        "basepower": basepower,
+        "status": status,
+        "brightness": brightness,
+        "_class": Light
+    }
