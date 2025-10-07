3f4737aa7c0bff3531595bcb1875d1941db0ea51
Julie <julietruc.dao@uzh.ch>
2025-10-07 14:35:06 +0200

    Correction of light_new so the inheritance works with Device

--- SoCo_HS25-group_004-a1/smart_house.py
+++ SoCo_HS25-group_004-a1/smart_house.py
@@ -1,9 +1,9 @@
-Device = {
-    "get_power_consumption": get_power_consumption,
-    "describe_device": describe_device,
-    "toggle_status": toggle_status,
-    "_classname": "Device"
-}
+def toggle_status(self):
+    if self["status"] == "on":
+        self["status"] = "off"
+    else:
+        self["status"] = "on"
+    return self["status"]
 
 def device_new(name, location, basepower, status):
     return {
@@ -14,6 +14,22 @@ def device_new(name, location, basepower, status):
         "_class": Device
     }
 
+Device = {
+    #"get_power_consumption": get_power_consumption,
+    #"describe_device": describe_device,
+    "toggle_status": toggle_status,
+    "_classname": "Device",
+    "_new": device_new
+}
+
+
+
+
+
+
+
+def make(cls, *args):
+    return cls["_new"](*args)
 
 
 
@@ -26,19 +42,20 @@ def light_consumption(self):
 def light_description(self):
     return f"The {self["name"]} is located in the {self["location"]}, is currently {self["status"]} and is currently set to {self["brightness"]}% brightness."
 
+def light_new(name, location, basepower, status, brightness):
+    return make(Device, name, location, basepower, status) | {
+            "brightness": brightness,
+            "_class": Light
+    }
+
 Light = {
     "get_power_consumption": light_consumption,
     "describe_device": light_description,
+    "_new": light_new,
     "_classname": "Light",
-    "_parent": Device
+    "_parent": Device,
 }
 
-def light_new(name, location, basepower, status, brightness):
-    return {
-        "name": name,
-        "location": location,
-        "basepower": basepower,
-        "status": status,
-        "brightness": brightness,
-        "_class": Light
-    }
+bedroom_light = make(Light, "Bedtable Light", "Bedroom", 300, "off", 70)
+print(light_description(bedroom_light))
+
